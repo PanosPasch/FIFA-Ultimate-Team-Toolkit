0f8503e7ab3d430689f2038fd268d5b7ecdfe228
Panagiotis Paschalis <paschalispan@gmail.com>
2016-01-19 22:41:40 +0200

    fixes to mobile client and model classes

--- UltimateTeam.Toolkit/Models/PurchasedItemsResponse.cs
+++ UltimateTeam.Toolkit/Models/PurchasedItemsResponse.cs
@@ -7,5 +7,13 @@ namespace UltimateTeam.Toolkit.Models
         public List<DuplicateItem> DuplicateItemIdList { get; set; }
 
         public List<ItemData> ItemData { get; set; }
+
+        public uint? Code { get; set; }
+
+        public string Debug { get; set; }
+
+        public string Reason { get; set; }
+
+        public string String { get; set; }
     }
 }
--- UltimateTeam.Toolkit/Models/WatchlistResponse.cs
+++ UltimateTeam.Toolkit/Models/WatchlistResponse.cs
@@ -11,5 +11,13 @@ namespace UltimateTeam.Toolkit.Models
         public List<DuplicateItem> DuplicateItemIdList { get; set; }
 
         public ushort Total { get; set; }
+
+        public uint? Code { get; set; }
+
+        public string Debug { get; set; }
+
+        public string Reason { get; set; }
+
+        public string String { get; set; }
     }
 }
